79cd64c12f9f08de197e2ffe20153c68356e3b9d
developer_interactly <developer.interactly@gmail.com>
2024-10-29 19:21:29 +0530

    added interview agent

--- main.py
+++ main.py
@@ -1,43 +1,128 @@
 from langchain_groq import ChatGroq
-from typing import Annotated, TypedDict
+from typing import Annotated, TypedDict, Optional
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from IPython.display import Image, display
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from pydantic import BaseModel
 
 from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatGroq(model="llama-3.2-90b-text-preview")
 
+class Candidate(BaseModel):
+    name: str
+    job_role: str
+
 class State(TypedDict):
-    messages: Annotated[list,add_messages]
+    history: Optional[str] = None
+    candidate_name: Optional[str] = None
+    role: Optional[str] = None
+    total_questions: Optional[int] = None
+    question: Optional[str] = None
+    answer: Optional[str] = None
 
-graph_builder = StateGraph(State)
+workflow = StateGraph(State)
 
-def chatbot(state: State):
-    return {"messages": llm.invoke(state['messages'])}
+prompt_extraction = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are an expert extraction algorithm. "
+            "Only extract relevant information from the text. "
+            "If you do not know the value of an attribute asked to extract, "
+            "return null for the attribute's value.",
+        ),
+        ("human", "{text}"),
+    ]
+)
 
-graph_builder.add_node('chatbot',chatbot)
-graph_builder.add_edge(START, 'chatbot')
-graph_builder.add_edge('chatbot', END)
+prompt_question = '''You are interviewer bot. Your name is Noha.
+You need to interview a candidate for role of {role}.
+This is a interview so far:
+{history}\n
+Ask your next question and dont repeat any question.'''
 
-graph = graph_builder.compile()
+prompt_response = '''Check whether the answer given for asked question is correct or not.
+Give a short response on it.
+question: {question}
+answer: {answer}'''
 
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant:", value["messages"].content)
+prompt_verdict = '''How do you feel about the overall interview given below?
+{history}'''
 
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
+def introduction(state: State):
+    intro = "Hi, I am Noha, can you please introduce yourself and what you do?"
+    print("Noha: ", intro)
+    question = "question: "+intro
+    return {"history":question,"question":question,"total_questions":1}
 
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+def handle_intro(state: State):
+    history = state.get('history', '').strip()
+    user_input = input("User: ")
+    runnable = prompt_extraction | llm.with_structured_output(schema=Candidate)
+    candidate = runnable.invoke({'text': user_input})
+    return {"history":history+'\n'+"User: "+user_input,"candidate_name":candidate.name, "role":candidate.job_role}
+
+def handle_question(state: State):
+    history = state.get('history', '').strip()
+    role = state.get('role', '').strip()
+
+    prompt = prompt_question.format(role=role, history=history)
+    question = llm.invoke(prompt).content
+    print("Noha: ", question)
+    return {"history":history+'\n'+question,"question":"question: "+question,"total_questions":state.get('total_questions')+1}
+
+def handle_answer(state: State):
+    history = state.get('history', '').strip()
+    user_input = input("User: ")
+    return {"history":history+'\n'+"user: "+user_input,"answer":user_input}
+
+def handle_response(state: State):
+    history = state.get('history', '').strip()
+    question = state.get('question', '').strip()
+    answer = state.get('answer', '').strip()
+
+    prompt = prompt_response.format(question=question, answer=answer)
+    response = llm.invoke(prompt).content
+    return {"history":history+'\n'+"evaluation: "+response}
+
+def handle_verdict(state: State):
+    history = state.get('history', '').strip()
+
+    prompt = prompt_verdict.format(history=history)
+    verdict = "question: "+llm.invoke(prompt).content
+    print("Noha: ",verdict)
+    return {"history":history+'\n'+verdict, "total_questions":state.get('total_questions')}
+
+def check_conv_length(state: State):
+    if state.get('total_questions') < 5:
+        return "handle_question"
+    else:
+        return "handle_verdict"
+    
+workflow.add_node("introduction", introduction)
+workflow.add_node("handle_intro", handle_intro)
+workflow.add_node("handle_question", handle_question)
+workflow.add_node("handle_answer", handle_answer)
+workflow.add_node("handle_response", handle_response)
+workflow.add_node("handle_verdict", handle_verdict)
+
+workflow.add_conditional_edges(
+    "handle_response",
+    check_conv_length,
+    {"handle_question":"handle_question","handle_verdict":"handle_verdict"}
+    )
+
+workflow.add_edge(START, "introduction")
+workflow.add_edge("introduction","handle_intro")
+workflow.add_edge("handle_intro","handle_question")
+workflow.add_edge("handle_question","handle_answer")
+workflow.add_edge("handle_answer","handle_response")
+workflow.add_edge("handle_verdict",END)
+
+app = workflow.compile()
+
+if __name__=="__main__":
+    app.invoke({"history":'',"candidate_name":'',"role":'',"total_questions":0,"question":'',"answer":''})
